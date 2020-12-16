a8c3241161152bd25b1e25488990a2e0ce827622
lzieberm <lzieberm@htl-steyr.ac.at>
2020-12-16 10:58:37 +0100

    Ticket + TicketController geändert

--- src/application/controller/TicketController.java
+++ src/application/controller/TicketController.java
@@ -1,9 +1,48 @@
 package application.controller;
 
-import javafx.scene.control.ListView;
-import javafx.scene.layout.AnchorPane;
+import application.model.Priority;
+import application.model.Status;
+import application.model.Ticket;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class TicketController {
-    public ListView ticketListView;
-    public AnchorPane contentPane;
+    public TextField ticketNameTextfield;
+    public TextField descriptionTextfield;
+    public ComboBox statiCombo;
+    public ComboBox priorityCombo;
+    private Ticket ticket = null;
+    /**
+     * darstellen der Daten des Tickets
+     */
+    public void setTicket(Ticket t){
+        ticketNameTextfield.setText(t.name);
+        descriptionTextfield.setText(t.description);
+        statiCombo.setItems(Status.load("stati.csv"));
+        priorityCombo.setItems(Priority.loadFile("priorities.csv"));
+
+        for (Status s : priorityCombo.getItems()){
+            if(s.number == t.status.number){
+                statiCombo.getSelectionModel().select(s);
+                break;
+            }
+        }
+
+        for (Priority p : priorityCombo.getItems()){
+            if (p.number == t.priority.number){
+                priorityCombo.getSelectionModel().select(p);
+                break;
+            }
+        }
+
+    }
+    public Ticket getTicket(){
+        /**
+         * aktualisieren der Ticket-Daten
+         */
+        ticket.name = ticketNameTextfield.getText();
+        //...
+        return ticket;
+    }
 }
--- src/application/model/Ticket.java
+++ src/application/model/Ticket.java
@@ -1,23 +1,30 @@
 package application.model;
 
 public class Ticket {
-    public String number;
+    //das is Person? kein Ticket
+    /**public String number;
     public String name;
     public String title;
     public String street;
     public String plz;
     public String ort;
     public String abteilungsnummer;
+     */
+    public String nummer;
+    public String name;
+    public String description;
+    public Status status;
+    public Priority priority;
 
   //  1;Fehlerbehebung Bezahlvorgang;Beim Abschließen einer Bestellung kommt es zu einer Nullpointer-Exception.;1;4
     //        2;Fertigstellen ToDo-System;Es soll jedes ToDo auf der rechten Seite angezeigt werden.;2;3
 
     public String toString() {
-        return number + " - " + title + name;
+        return nummer + " - " + name ;
     }
 
     public String newCSVLine() {
-        return number + "\";\"" + title + "\";\"" + name + "\";\"" + street + "\";\"" +
-                plz + "\";\"" + ort + "\";\"" + abteilungsnummer + "\";\"";
+        //1;Fehlerbehebung Bezahlvorgang;Beim Abschließen einer Bestellung kommt es zu einer Nullpointer-Exception.;1;4
+        return nummer + "\";\"" + name + "\";\"" + description +  "\";\"" + status +  "\";\"" + priority +  "\";\"";
     }
 }
